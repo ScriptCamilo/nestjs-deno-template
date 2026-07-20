5c2556c49c5b7aaea627d74f4264aed03f109e34
ScriptCamilo <script.camilo@gmail.com>
2026-07-20 17:26:42 -0300

    feat: update greeting message in AppController test for Deno Deploy integration

--- src/app.controller.test.ts
+++ src/app.controller.test.ts
@@ -18,8 +18,10 @@ describe("AppController", () => {
   });
 
   describe("root", () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe("Hello World!");
+    it('should return "Hello World, from Deno Deploy with NestJS!"', () => {
+      expect(appController.getHello()).toBe(
+        "Hello World, from Deno Deploy with NestJS!",
+      );
     });
   });
 });
